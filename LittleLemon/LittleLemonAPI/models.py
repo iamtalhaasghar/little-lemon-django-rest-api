bf658e5d84e49073e275291226b013540b7684f0
Talha Asghar <talhaasghar220@gmail.com>
2023-05-05 12:37:52 +0500

    created MenuItem model

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+
+    name = models.TextField()
+    price = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
